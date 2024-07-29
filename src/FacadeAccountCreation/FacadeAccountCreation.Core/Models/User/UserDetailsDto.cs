c2fdd480d317db64805c1580966aa22147e11ae4
MasilEviden <masil.puveendran@eviden.com>
2024-07-29 22:04:54 +0100

    Annotated UserDetailsDto

--- src/FacadeAccountCreation/FacadeAccountCreation.Core/Models/User/UserDetailsDto.cs
+++ src/FacadeAccountCreation/FacadeAccountCreation.Core/Models/User/UserDetailsDto.cs
@@ -2,15 +2,30 @@
 
 namespace FacadeAccountCreation.Core.Models.User
 {
+    /// <summary>
+    /// User details that can be updated
+    /// </summary>
     [ExcludeFromCodeCoverage]
     public record UserDetailsDto
     {
+        /// <summary>
+        /// User Firstname
+        /// </summary>
         public string FirstName { get; init; }
 
+        /// <summary>
+        /// User Lastname
+        /// </summary>
         public string LastName { get; init; }
 
+        /// <summary>
+        /// User Jobtitle in an organisation
+        /// </summary>
         public string JobTitle { get; init; }
 
+        /// <summary>
+        /// User Telephone
+        /// </summary>
         public string TelePhone { get; init; }
     }
 }
