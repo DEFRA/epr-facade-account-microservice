50cae4891a13fce3f0ae61c5daa9f0f55c592391
Raveendra Attota <47723463+RaveendraAttota@users.noreply.github.com>
2024-10-16 11:47:19 +0100

    Added request and response messages for producer fee calculation

---
+++ src/FacadeAccountCreation/FacadeAccountCreation.Core/Models/Organisations/PaymentCalculationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FacadeAccountCreation.Core.Models.Organisations
+{
+    [ExcludeFromCodeCoverage]
+    public class PaymentCalculationRequest
+    {
+        public string ProducerType { get; set; }
+        public int NumberOfSubsidiaries { get; set; }
+        public int Regulator { get; set; }
+        public int NoOfSubsidiariesOnlineMarketplace { get; set; }
+        public bool IsProducerOnlineMarketplace { get; set; }
+        public string ApplicationReferenceNumber { get; set; }
+    }
+}
---
+++ src/FacadeAccountCreation/FacadeAccountCreation.Core/Models/Organisations/PaymentCalculationResponse.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FacadeAccountCreation.Core.Models.Organisations
+{
+    [ExcludeFromCodeCoverage]
+    public class PaymentCalculationResponse
+    {
+        public decimal ProducerRegistrationFee { get; set; }
+        public decimal ProducerOnlineMarketPlaceFee { get; set; }
+        public decimal SubsidiariesFee { get; set; }
+        public decimal TotalFee { get; set; }
+        public decimal PreviousPayment { get; set; }
+        public decimal OutstandingPayment { get; set; }
+        public SubsidiariesFeeBreakdown SubsidiariesFeeBreakdown { get; set; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class SubsidiariesFeeBreakdown
+    {
+        public decimal TotalSubsidiariesOMPFees { get; set; }
+        public int CountOfOMPSubsidiaries { get; set; }
+        public decimal UnitOMPFees { get; set; }
+        public FeeBreakdown FeeBreakdowns { get; set; }
+    }
+
+    [ExcludeFromCodeCoverage]
+    public class FeeBreakdown
+    {
+        public int BandNumber { get; set; }
+        public int UnitCount { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
