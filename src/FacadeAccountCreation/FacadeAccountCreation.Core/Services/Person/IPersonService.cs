da701306510413afbce3fb04680629562ce1017e
Stuart Wilson <179944599+swilson-eviden@users.noreply.github.com>
2025-01-29 08:55:36 +0000

    458556 Resubmit org and pom files now displays deleted user instead of erroring (#85)

--- src/FacadeAccountCreation/FacadeAccountCreation.Core/Services/Person/IPersonService.cs
+++ src/FacadeAccountCreation/FacadeAccountCreation.Core/Services/Person/IPersonService.cs
@@ -5,7 +5,7 @@ namespace FacadeAccountCreation.Core.Services.Person;
 public interface IPersonService
 {
     Task<PersonResponseModel?> GetPersonByUserIdAsync(Guid userId);
-        
+    Task<PersonResponseModel?> GetAllPersonByUserIdAsync(Guid userId);
     Task<PersonResponseModel?> GetPersonByExternalIdAsync(Guid externalId);
     Task<InviteApprovedUserModel> GetPersonByInviteToken(string token);
 }
