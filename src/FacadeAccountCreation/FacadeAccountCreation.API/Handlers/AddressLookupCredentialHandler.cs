142e98dc1a727cd9d87e7d7b3c46894898a016b6
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-02-13 13:48:40 +0000

    Boomi v2 oauth (#82) (#86)
    Had to fix some using statements as main/R8.0  has been refactored with GlobalUsings
    * initial POC
    
    * final fixes
    
    * fixes for co house
    
    * co house fixes
    
    * fixed unit test
    
    * rebuild for PR
    
    * Remove obsolete code, exclude handlers from sonar chacks
    
    * Add new boomi settings to appsettings.json
    
    * Add boomi feature flag, put certificate back
    
    ---------
    
    Co-authored-by: Mike Wild <Michael.Wild@defra.onmicrosoft.com>
    # Conflicts:
    #	src/FacadeAccountCreation/FacadeAccountCreation.API/Extensions/HttpClientServiceCollectionExtension.cs
    #	src/FacadeAccountCreation/FacadeAccountCreation.Core/Services/CompaniesHouse/CompaniesHouseLookupService.cs
    #	src/FacadeAccountCreation/FacadeAccountCreation.UnitTests/Core/Services/CompaniesHouseServiceTests.cs

---
+++ src/FacadeAccountCreation/FacadeAccountCreation.API/Handlers/AddressLookupCredentialHandler.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using FacadeAccountCreation.API.Configs;
+using Microsoft.Extensions.Options;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
+
+namespace FacadeAccountCreation.API.Handlers;
+
+[ExcludeFromCodeCoverage]
+public class AddressLookupCredentialHandler : DelegatingHandler
+{
+    private readonly TokenRequestContext _tokenRequestContext;
+    private readonly ClientSecretCredential _credentials;
+
+    public AddressLookupCredentialHandler(IOptions<ApiConfig> options)
+    {
+        var clientApiOptions = options.Value;
+        _tokenRequestContext = new TokenRequestContext(new[] { clientApiOptions.AddressLookupScope });
+        _credentials = new ClientSecretCredential(clientApiOptions.TenantId, clientApiOptions.ClientId, clientApiOptions.ClientSecret);
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var tokenResult = await _credentials.GetTokenAsync(_tokenRequestContext, cancellationToken);
+        request.Headers.Authorization = new AuthenticationHeaderValue(Constants.Client.Bearer, tokenResult.Token);
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
