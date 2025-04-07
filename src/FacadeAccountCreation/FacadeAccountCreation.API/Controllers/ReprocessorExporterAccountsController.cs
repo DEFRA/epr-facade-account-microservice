ba71413931626ea056f2e064ec495fd3c0bf3c49
Phil Davies <203141900+phil-davies-eviden@users.noreply.github.com>
2025-04-07 10:20:02 +0100

    tidy up

--- src/FacadeAccountCreation/FacadeAccountCreation.API/Controllers/ReprocessorExporterAccountsController.cs
+++ src/FacadeAccountCreation/FacadeAccountCreation.API/Controllers/ReprocessorExporterAccountsController.cs
@@ -13,22 +13,6 @@ public class ReprocessorExporterAccountsController(IAccountService accountServic
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> CreateAccount(ReprocessorExporterAccountModel account)
     {
-        //todo: can person contact email and user email be different? don't think do, but check source (frontend) - we don't capture the person email during the journey
-        // if they can be different, might need something like this..
-        //var accountWithUser = new AccountWithUserModel(account, new UserModel
-        //{
-        //    UserId = User.UserId(),
-        //    Email = User.Email()
-        //});
-
-        //var createAccountResponse = await accountService.AddReprocessorExporterAccountAsync(account);
-
-        //if (createAccountResponse == null)
-        //{
-        //    //todo: pass root issue, so consumers don't have to dig through logs?
-        //    return Problem("Failed to create reprocessor/exporter account", statusCode: StatusCodes.Status500InternalServerError);
-        //}
-
         // we could just send down the user id guid, rather than an user, as the email is currently always the same as the person email and the other fields are not populated
         // but we follow the existing account creation pattern, which sends down a User
         var accountWithUser = new ReprocessorExporterAccountWithUserModel(account, new UserModel
@@ -48,6 +32,12 @@ public class ReprocessorExporterAccountsController(IAccountService accountServic
 
         //todo: if issue, what happens and do we need to return a Problem?
         await accountService.AddReprocessorExporterAccountAsync(accountWithUser);
+
+        //if (createAccountResponse == null)
+        //{
+        //    //todo: pass root issue, so consumers don't have to dig through logs?
+        //    return Problem("Failed to create reprocessor/exporter account", statusCode: StatusCodes.Status500InternalServerError);
+        //}
 
         //todo: we need to send a confirmation, but we don't have an email template yet
         //messagingService.SendAccountCreationConfirmation(
