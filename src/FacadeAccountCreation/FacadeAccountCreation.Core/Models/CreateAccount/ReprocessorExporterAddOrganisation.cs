3b6d9dbda3c23d48c574a401bfee66d215d564ba
Sharjeel Baloch <sharjeel.baloch.external@eviden.com>
2025-06-06 16:45:07 +0100

    changes so far

--- src/FacadeAccountCreation/FacadeAccountCreation.Core/Models/CreateAccount/ReprocessorExporterAddOrganisation.cs
+++ src/FacadeAccountCreation/FacadeAccountCreation.Core/Models/CreateAccount/ReprocessorExporterAddOrganisation.cs
@@ -6,9 +6,11 @@ public class ReprocessorExporterAddOrganisation
     public ReprocessorExporterUserModel User { get; set; } = null!;
 
     [Required]
-    public OrganisationModel Organisation { get; set; } = null!;
+    public OrganisationModel? Organisation { get; set; } = null; // To DO: need this null if manual input got values
 
-    public List<PartnerModel> Partners { get; set; } = new();
+    public ReExManualInputModel? ManualInput { get; set; } // To DoL: need this null if Organisation has company data
+
+    public List<PartnerModel> Partners { get; set; } = [];
 
     public List<InvitedApprovedUserModel> InvitedApprovedUsers { get; set; } = [];
 
