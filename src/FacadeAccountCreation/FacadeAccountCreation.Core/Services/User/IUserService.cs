9bb66df5ff6044aa61fdb4e0ad9fbd8d3564b02d
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-09-10 15:15:24 +0100

    Add GetUserIdForPersonId endpoint AB#607760 (#143)
    * Add calls to GetUserIdByPersonId to support Graph API in a/c management
    
    * Fix GetUserIdByPersonId return type
    
    * Fix GetUserIdByPersonId unit test
    
    * Fix typo - add missing space
    
    ---------
    
    Co-authored-by: Mike Wild <Michael.Wild@defra.onmicrosoft.com>

--- src/FacadeAccountCreation/FacadeAccountCreation.Core/Services/User/IUserService.cs
+++ src/FacadeAccountCreation/FacadeAccountCreation.Core/Services/User/IUserService.cs
@@ -3,10 +3,14 @@ namespace FacadeAccountCreation.Core.Services.User;
 public interface IUserService
 {
     Task<HttpResponseMessage> GetUserOrganisations(Guid userId);
+
     Task<HttpResponseMessage> GetUserOrganisations(Guid userId, string serviceKey);
+
+    Task<HttpResponseMessage> GetUserIdByPersonId(Guid personId);
+
     Task<HttpResponseMessage> UpdatePersonalDetailsAsync(
-        Guid userId, 
-        Guid organisationId, 
-        string serviceKey, 
+        Guid userId,
+        Guid organisationId,
+        string serviceKey,
         UpdateUserDetailsRequest userDetailsUpdateModelRequest);
 }
